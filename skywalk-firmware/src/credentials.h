6c219dcc88b536428b6509be45b14d8eec85bf54
Sagamurugan <sagamurugans.cse2024@citchennai.net>
2025-08-30 13:55:11 +0530

    updated firmware code

---
+++ skywalk-firmware/src/credentials.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID "Airtel_sakthi5G"
+#define WIFI_PASSWORD "Saravana@1234"
+
+
+#define FIREBASE_API_KEY "AIzaSyDzfj0gKtKkvuOxuwQ1TyD4XaH2mb9H4r0"
+#define FIREBASE_PROJECT_ID "mr-skywalk-dashboard"
+
+
+#define FIREBASE_USER_EMAIL "sagamurugan7@gmail.com"
+#define FIREBASE_USER_PASSWORD "Saga@2006"
